2913d62c8fa829f93a8e23bd5f0f4d1ca320ee40
Yankey N'wollé Ange Christian <168791054+Yankeyange@users.noreply.github.com>
2024-10-21 07:00:28 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 
 /* pour le premier bouton (1) */
+
+/* permet de sélectionner le bouton avec la classe number1 et le querySelector permet de cibler spécifiquement cet éléments */
 let button1 = document.querySelector(".number1");
+/* ajout d'un écouteur d'évènement et le click permet de déclencher la fonction  */
 button1.addEventListener("click",Message);
 
-
+/* on crée une fonctio Message et le messageElement.innerHTML permet de modifier le contenu */
 function Message() {
   const messageElement = document.querySelector('#message');
   messageElement.innerHTML = "You selected 1 out of 5. Thank you!  We appreciate you taking the time to give a rating.   If you ever need more support,  don’t hesitate to get in touch!";
